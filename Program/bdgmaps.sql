f2c7bb7641c88dc2ea9fde7012dc94d0919fd846
hilmi-a <hilmi.a.aziz@gmail.com>
2015-05-29 21:36:31 +0700

    edit .sql

--- Program/bdgmaps.sql
+++ Program/bdgmaps.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2015 at 10:37 AM
+-- Generation Time: May 29, 2015 at 04:34 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -33,19 +33,12 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `password_admin` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `desa`
+-- Dumping data for table `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `desa` (
-`id_desa` int(11) NOT NULL,
-  `nama_desa` varchar(20) NOT NULL,
-  `id_kecamatan` int(11) NOT NULL,
-  `lat` varchar(25) NOT NULL,
-  `long` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `admin` (`username_admin`, `password_admin`) VALUES
+('admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -63,13 +56,26 @@ CREATE TABLE IF NOT EXISTS `kecamatan` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `kelurahan`
+--
+
+CREATE TABLE IF NOT EXISTS `kelurahan` (
+`id_kelurahan` int(11) NOT NULL,
+  `nama_kelurahan` varchar(20) NOT NULL,
+  `id_kecamatan` int(11) NOT NULL,
+  `lat` varchar(25) NOT NULL,
+  `long` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pengusaha`
 --
 
 CREATE TABLE IF NOT EXISTS `pengusaha` (
 `id_pengusaha` int(11) NOT NULL,
   `nama_pengusaha` varchar(20) NOT NULL,
-  `username` varchar(17) NOT NULL,
   `email` varchar(30) NOT NULL,
   `password` varchar(16) NOT NULL,
   `username_admin` varchar(11) NOT NULL
@@ -109,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `usaha` (
   `id_pengusaha` int(11) NOT NULL,
   `produk_utama` varchar(30) NOT NULL,
   `alamat_usaha` varchar(50) NOT NULL,
-  `id_desa` int(11) NOT NULL,
+  `id_kelurahan` int(11) NOT NULL,
   `id_kecamatan` int(11) NOT NULL,
   `telp` varchar(13) NOT NULL,
   `lat` varchar(25) NOT NULL,
@@ -135,16 +141,16 @@ ALTER TABLE `admin`
  ADD PRIMARY KEY (`username_admin`);
 
 --
--- Indexes for table `desa`
---
-ALTER TABLE `desa`
- ADD PRIMARY KEY (`id_desa`), ADD KEY `id_kecamatan` (`id_kecamatan`);
-
---
 -- Indexes for table `kecamatan`
 --
 ALTER TABLE `kecamatan`
  ADD PRIMARY KEY (`id_kecamatan`);
+
+--
+-- Indexes for table `kelurahan`
+--
+ALTER TABLE `kelurahan`
+ ADD PRIMARY KEY (`id_kelurahan`), ADD KEY `id_kecamatan` (`id_kecamatan`);
 
 --
 -- Indexes for table `pengusaha`
@@ -168,22 +174,22 @@ ALTER TABLE `skala_usaha`
 -- Indexes for table `usaha`
 --
 ALTER TABLE `usaha`
- ADD PRIMARY KEY (`id_usaha`), ADD KEY `kecamatan` (`id_kecamatan`), ADD KEY `kecamatan_2` (`id_kecamatan`), ADD KEY `id_pengusaha` (`id_pengusaha`), ADD KEY `id_desa` (`id_desa`), ADD KEY `id_skala` (`id_skala`), ADD KEY `id_sektor` (`id_sektor`);
+ ADD PRIMARY KEY (`id_usaha`), ADD KEY `kecamatan` (`id_kecamatan`), ADD KEY `kecamatan_2` (`id_kecamatan`), ADD KEY `id_pengusaha` (`id_pengusaha`), ADD KEY `id_desa` (`id_kelurahan`), ADD KEY `id_skala` (`id_skala`), ADD KEY `id_sektor` (`id_sektor`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `desa`
---
-ALTER TABLE `desa`
-MODIFY `id_desa` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `kecamatan`
 --
 ALTER TABLE `kecamatan`
 MODIFY `id_kecamatan` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `kelurahan`
+--
+ALTER TABLE `kelurahan`
+MODIFY `id_kelurahan` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `pengusaha`
 --
@@ -199,10 +205,10 @@ MODIFY `id_usaha` int(11) NOT NULL AUTO_INCREMENT;
 --
 
 --
--- Constraints for table `desa`
+-- Constraints for table `kelurahan`
 --
-ALTER TABLE `desa`
-ADD CONSTRAINT `desa_ibfk_1` FOREIGN KEY (`id_kecamatan`) REFERENCES `kecamatan` (`id_kecamatan`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `kelurahan`
+ADD CONSTRAINT `kelurahan_ibfk_1` FOREIGN KEY (`id_kecamatan`) REFERENCES `kecamatan` (`id_kecamatan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `pengusaha`
@@ -215,9 +221,9 @@ ADD CONSTRAINT `pengusaha_ibfk_1` FOREIGN KEY (`username_admin`) REFERENCES `adm
 --
 ALTER TABLE `usaha`
 ADD CONSTRAINT `usaha_ibfk_1` FOREIGN KEY (`id_pengusaha`) REFERENCES `pengusaha` (`id_pengusaha`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `usaha_ibfk_3` FOREIGN KEY (`id_desa`) REFERENCES `desa` (`id_desa`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `usaha_ibfk_4` FOREIGN KEY (`id_skala`) REFERENCES `skala_usaha` (`id_skala`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `usaha_ibfk_5` FOREIGN KEY (`id_sektor`) REFERENCES `sektor_usaha` (`id_sektor`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `usaha_ibfk_5` FOREIGN KEY (`id_sektor`) REFERENCES `sektor_usaha` (`id_sektor`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `usaha_ibfk_6` FOREIGN KEY (`id_kelurahan`) REFERENCES `kelurahan` (`id_kelurahan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
