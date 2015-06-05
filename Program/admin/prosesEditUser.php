c9491a6cdb444370381472b7229942414ea9c2e7
frostyfos <winata.nando@gmail.com>
2015-06-05 12:54:48 +0700

    tambah pas edit kalo ga ada img yang di upload make img default (ga d upload, udah ada di folder gambar/defaul.jpg)

--- Program/admin/prosesEditUser.php
+++ Program/admin/prosesEditUser.php
@@ -21,25 +21,25 @@
 	$status_akun = $_POST['status_akun'];
 	//$encrypted = md5($password); // Encrypting pssword using md5 algo
 	$ttl = $_POST['ttl'];
-	
+		if($_FILES['userfile']['error']==4){
+			$FileUpload = "default.jpg";
+		}
 		if($_FILES['userfile']['error']==0){
 			$namafilebaru="../gambar/".$_FILES['userfile']['name'];
 		
 			if(move_uploaded_file($_FILES['userfile']['tmp_name'],
 				$namafilebaru)==true){
+				$FileUpload = $_FILES['userfile']['name'];
 				echo "File telah tersimpan.";
 			}
 				else{
 					echo "Gagal menyimpan file upload";
 				}
-				
-		}else if($_FILES['userfile']['error']==4){
-			echo 'ga ada yang di upload';
 		}
 		else
 		echo "Gagal Upload";
 		
-		$filektp = "gambar/".$_FILES['userfile']['name'];
+		$filektp = "gambar/".$FileUpload;
 		
 			$sql = "UPDATE pengusaha SET no_ktp = '$username', nama_pengusaha = '$nama', alamat = '$alamat', ttl = '$ttl', jenis_kelamin = '$jk', file_ktp = '$filektp', email='$email', password='$password', status_akun = '$status_akun' WHERE id_pengusaha = '$id_pengusaha'";
 		
